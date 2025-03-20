5b42ef9ca4ee0c272027d9206979471f7c93b2da
kavindu919 <jayakodyk2@gmail.com>
2025-03-20 22:33:23 +0530

    do change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import DamageClaimForm from "./pages/DamageClaimForm";
 import LoginPage from "./pages/LoginPage";
-import Refundtable from "./components/Refundtable";
+import Refund from "./pages/Refund";
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/claimpage" element={<DamageClaimForm />} />
-        <Route path="/refundtable" element={<Refundtable />} />
+        <Route path="/refundtable" element={<Refund />} />
       </Routes>
     </Router>
   );
